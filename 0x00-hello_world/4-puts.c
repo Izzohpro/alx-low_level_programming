5351ff6adc4ad21dbc5b6cca788379425b9316d3
Izzohpro <okparafrankline@gmail.com>
2023-03-16 12:34:01 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main fuction
+ * main - this is the main function
  *
  * return: always 0
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
