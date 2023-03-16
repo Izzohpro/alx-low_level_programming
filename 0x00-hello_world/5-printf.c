34d3a441322ff36864e5288883bd8493a5d68da7
Izzohpro <okparafrankline@gmail.com>
2023-03-16 13:04:53 -0700

    5 print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - my main function
+ *
+ * return: 0 when successful 
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
