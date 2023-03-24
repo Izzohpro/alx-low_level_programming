ea3f8b3be5f1f2deeec0963628785c1275f738ea
Izzohpro <okparafrankline@gmail.com>
2023-03-24 15:08:50 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,17 @@
 
 void print_most_numbers(void)
 {
-	int c = 0;
-	for (; c <= 9; c++)
-	{
-	if (c == 2 || c == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(c + '0');
-	}
-	}
-	_putchar('\n');
+int c = 0;
+for (; c <= 9; c++)
+{
+if (c == 2 || c == 4)
+{
+continue;
+}
+else
+{
+_putchar(c + '0');
+}
+}
+_putchar('\n');
 }
