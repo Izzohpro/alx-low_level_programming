836120cec3bd543fabde674e9d34ee0f99f3ae09
Izzohpro <okparafrankline@gmail.com>
2023-04-10 03:05:15 -0700

    Library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Entry point
+ * @str: string to input
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+
+{
+
+int i = 0;
+
+while (str[i])
+
+{
+
+_putchar(str[i]);
+
+i++;
+
+}
+
+_putchar('\n');
+
+}
