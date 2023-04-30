4d8e54a4a1dad6f60794423be96e7854d3d7920c
Izzohpro <okparafrankline@gmail.com>
2023-04-29 23:14:50 -0700

    length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
+ * listint_len - returns number of the elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
- * Return: number of nodes
+ * Return: nodes number
  */
 size_t listint_len(const listint_t *h)
 {
